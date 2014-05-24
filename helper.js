a2a7e6af3030601b2244d953f59edbce39c2643d
Ulrich Schmidt <ulrich.schmidt@sevenval.com>
2014-05-24 08:57:10 +0200

    fixing the config for live deplyoment

--- helper.js
+++ helper.js
@@ -1,18 +1,24 @@
+var github = {};
+
+try {
+  github = require('./github.json');
+} catch (e) {
+  github = {  
+    GITHUB_CLIENT_ID : "",
+    GITHUB_CLIENT_SECRET : ""
+  }
+}
 
 var config = {
   "github" : {
-    "GITHUB_CLIENT_ID" : process.env.GITHUB_CLIENT_ID, 
-    "GITHUB_CLIENT_SECRET" : process.env.GITHUB_CLIENT_SECRET,
-    "callbackURL" : process.env.callbackURL
+    "GITHUB_CLIENT_ID" : process.env.GITHUB_CLIENT_ID ? process.env.GITHUB_CLIENT_ID : github.GITHUB_CLIENT_ID, 
+    "GITHUB_CLIENT_SECRET" : process.env.GITHUB_CLIENT_SECRET ? process.env.GITHUB_CLIENT_SECRET : github.GITHUB_CLIENT_SECRET,
+    "callbackURL" : process.env.callbackURL ? process.env.callbackURL : "http://localhost:3000/users/auth/github/callback"
   },
-  "mongourl" : process.env.mongodburl
+  "mongourl" : process.env.mongodburl ? process.env.mongodburl : "mongodb://localhost:27017/odlthek"
 }
 
-
 var db = require('mongoskin').db(config.mongourl);
-
-
-
 
 
 /* test if the user ist authorized */
