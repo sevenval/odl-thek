4c76a7d0a4099844f28e887a054720f97ae3a053
Ulrich Schmidt <ulrich.schmidt@sevenval.com>
2014-05-25 01:13:07 +0200

    last user hinzugefügt

--- routes/gadgets.js
+++ routes/gadgets.js
@@ -6,57 +6,94 @@ var users = helper.db.collection('users');
 
 var router = express.Router();
 
-router.get('/', helper.ensureAuthenticated,  function(req, res) {
+
+router.get('/',   function(req, res) { //helper.ensureAuthenticated,
   gadgets.find({ type :  "mobile" }).sort({brand:1}).toArray(function(_err,_result){
-    if(_result&&_result!=undefined&&_result.length) {	  
-		bookings.find({ start: {$lte: new Date()}, end: {$gte: new Date()} }).toArray(function(_err,_booked_result){
-			if(_booked_result&&_booked_result!=undefined) {
-				//console.log(_booked_result[0]);
-				//console.log(_result[0]);
-				for(i = 0;i < _result.length;i++) {
-					_result[i].booked = false;
-					_result[i].handout = false;
-					_result[i].lastuser = false;
-					//bookings.find({ gadget:_result[i]._id }).sort({end:-1}).limit(1).toArray(function(_err,_last_booking_result){
-					//	if(_last_booking_result&&_last_booking_result!=undefined&&_last_booking_result.length) {
-					//		console.log(_last_booking_result[0]);
-					//		_result[i].lastuser = _last_booking_result[0].username;
-					//	}
-					//})
-					for(j = 0;j < _booked_result.length;j++) {
-						if(_result[i]._id == _booked_result[j].gadget && _booked_result[j].status == "handout") {
-							_result[i].handout = true;
-							users.findById( _booked_result[j].user, function(_err,_users_result){
-								if(_users_result&&_users_result!=undefined&&_users_result.length) {
-									if(_users_result[0].name) {
-										_result[i].lastuser = _users_result[0].name;
-									} else if (_users_result[0].displayname) {
-										_result[i].lastuser = _users_result[0].name;
-									}
-								}
-							});
-						} else if(_result[i]._id == _booked_result[j].gadget){
-							_result[i].booked = true;
-							users.findById( _booked_result[j].user, function(_err,_users_result){
-								if(_users_result&&_users_result!=undefined&&_users_result.length) {
-									if(_users_result[0].name) {
-										_result[i].lastuser = _users_result[0].name;
-									} else if (_users_result[0].displayname) {
-										_result[i].lastuser = _users_result[0].name;
-									}
-								}
-							});
-						}
-					}
-				}	
-			}
-			res.render('gadgets/list', { title: 'Gadgets: '+_result.length, gadgets : _result});  
-		})
+    if(_result&&_result!=undefined&&_result.length) {    
+    bookings.find({}).toArray(function(_err,_bookings){
+      var book = {};
+      var now = new Date();
+      for(var i = 0; i < _bookings.length ; i++) {
+        if(!book[_bookings[i].gadget]) {
+          book[_bookings[i].gadget] = {
+            last : null,
+            current : null
+          };
+        }
+        if(_bookings[i].end < now) { // todo nur übernehmen, wenn wirklich das letzte.
+          book[_bookings[i].gadget].last = _bookings[i];
+        }
+        if(_bookings[i].start < now && _bookings[i].end > now) {
+          book[_bookings[i].gadget].current = _bookings[i]; 
+        }
+      }
+      for(var i = 0 ; i < _result.length;i++) {
+        _result[i].bookings = book[_result[i]._id];
+      }
+      console.log(_result[0]);
+      res.render('gadgets/list', { title: 'Gadgets: '+_result.length, gadgets : _result});  
+    })
     } else {
       res.render('index', { title: 'No devices' });  
     }
   })
 });
+
+
+// router.get('/',   function(req, res) { //helper.ensureAuthenticated,
+//   gadgets.find({ type :  "mobile" }).sort({brand:1}).toArray(function(_err,_result){
+//     if(_result&&_result!=undefined&&_result.length) {	  
+// 		bookings.find({ start: {$lte: new Date()}, end: {$gte: new Date()} }).toArray(function(_err,_booked_result){
+// 			if(_booked_result&&_booked_result!=undefined) {
+// 				//console.log(_booked_result[0]);
+// 				//console.log(_result[0]);
+// 				for(i = 0;i < _result.length;i++) {
+// 					_result[i].booked = false;
+// 					_result[i].handout = false;
+// 					_result[i].lastuser = false;
+
+// 					// bookings.find({ gadget : _result[i]._id.toString() }).sort({end:-1}).limit(1).toArray(function(_err,_last_booking_result){
+//      //        if(_last_booking_result&&_last_booking_result!=undefined&&_last_booking_result.length) {
+// 					// 		console.log(_last_booking_result[0]);
+// 					// 		_result[i].lastuser = _last_booking_result[0].username;
+// 					// 	}
+// 					// })
+// 					for(j = 0;j < _booked_result.length;j++) {
+//             /*rot*/
+// 						if(_result[i]._id == _booked_result[j].gadget && _booked_result[j].status == "handout") {
+// 							_result[i].handout = true;
+// 							users.findById( _booked_result[j].user, function(_err,_users_result){
+// 								if(_users_result&&_users_result!=undefined&&_users_result.length) {
+// 									if(_users_result[0].name) {
+// 										_result[i].lastuser = _users_result[0].name;
+// 									} else if (_users_result[0].displayname) {
+// 										_result[i].lastuser = _users_result[0].name;
+// 									}
+// 								}
+// 							});
+//             /* gelb */
+// 						} else if(_result[i]._id == _booked_result[j].gadget){
+// 							_result[i].booked = true;
+// 							users.findById( _booked_result[j].user, function(_err,_users_result){
+// 								if(_users_result&&_users_result!=undefined&&_users_result.length) {
+// 									if(_users_result[0].name) {
+// 										_result[i].lastuser = _users_result[0].name;
+// 									} else if (_users_result[0].displayname) {
+// 										_result[i].lastuser = _users_result[0].name;
+// 									}
+// 								}
+// 							});
+// 						}
+// 					}
+// 				}	
+// 			}
+// 			res.render('gadgets/list', { title: 'Gadgets: '+_result.length, gadgets : _result});  
+// 		})
+//     } else {
+//       res.render('index', { title: 'No devices' });  
+//     }
+//   })
+// });
 
 
 
