3ccebd4f64d9ebc0a2911fbb491d7483530c89e7
Ulrich Schmidt <ulrich.schmidt@sevenval.com>
2014-05-25 10:26:58 +0200

    edit, add gadgets, little layout

--- routes/gadgets.js
+++ routes/gadgets.js
@@ -7,8 +7,13 @@ var users = helper.db.collection('users');
 var router = express.Router();
 
 
-router.get('/',   function(req, res) { //helper.ensureAuthenticated,
-  gadgets.find({ type :  "mobile" }).sort({brand:1}).toArray(function(_err,_result){
+
+router.get('/',  helper.ensureAuthenticated, function(req, res) { 
+  var find = { type :  "mobile" };
+  if(req.query.q) {
+    find.name = {$regex : ".*"+req.query.q+".*", $options: 'i'};
+  }
+  gadgets.find(find).sort({brand:1}).toArray(function(_err,_result){
     if(_result&&_result!=undefined&&_result.length) {    
     bookings.find({}).toArray(function(_err,_bookings){
       var book = {};
@@ -39,68 +44,28 @@ router.get('/',   function(req, res) { //helper.ensureAuthenticated,
   })
 });
 
+router.get('/new', helper.ensureAuthenticated, helper.ensureAdmin, function(req, res) { // 
+  var basic = { 
+  hwid: '',
+  name: '',
+  available: '',
+  location: '',
+  description: '',
+  brand: '',
+  model: '',
+  os: '',
+  type: ''}
+    res.render('gadgets/new', { title: "new gadget" , gadget : basic});
+});
 
-// router.get('/',   function(req, res) { //helper.ensureAuthenticated,
-//   gadgets.find({ type :  "mobile" }).sort({brand:1}).toArray(function(_err,_result){
-//     if(_result&&_result!=undefined&&_result.length) {	  
-// 		bookings.find({ start: {$lte: new Date()}, end: {$gte: new Date()} }).toArray(function(_err,_booked_result){
-// 			if(_booked_result&&_booked_result!=undefined) {
-// 				//console.log(_booked_result[0]);
-// 				//console.log(_result[0]);
-// 				for(i = 0;i < _result.length;i++) {
-// 					_result[i].booked = false;
-// 					_result[i].handout = false;
-// 					_result[i].lastuser = false;
-
-// 					// bookings.find({ gadget : _result[i]._id.toString() }).sort({end:-1}).limit(1).toArray(function(_err,_last_booking_result){
-//      //        if(_last_booking_result&&_last_booking_result!=undefined&&_last_booking_result.length) {
-// 					// 		console.log(_last_booking_result[0]);
-// 					// 		_result[i].lastuser = _last_booking_result[0].username;
-// 					// 	}
-// 					// })
-// 					for(j = 0;j < _booked_result.length;j++) {
-//             /*rot*/
-// 						if(_result[i]._id == _booked_result[j].gadget && _booked_result[j].status == "handout") {
-// 							_result[i].handout = true;
-// 							users.findById( _booked_result[j].user, function(_err,_users_result){
-// 								if(_users_result&&_users_result!=undefined&&_users_result.length) {
-// 									if(_users_result[0].name) {
-// 										_result[i].lastuser = _users_result[0].name;
-// 									} else if (_users_result[0].displayname) {
-// 										_result[i].lastuser = _users_result[0].name;
-// 									}
-// 								}
-// 							});
-//             /* gelb */
-// 						} else if(_result[i]._id == _booked_result[j].gadget){
-// 							_result[i].booked = true;
-// 							users.findById( _booked_result[j].user, function(_err,_users_result){
-// 								if(_users_result&&_users_result!=undefined&&_users_result.length) {
-// 									if(_users_result[0].name) {
-// 										_result[i].lastuser = _users_result[0].name;
-// 									} else if (_users_result[0].displayname) {
-// 										_result[i].lastuser = _users_result[0].name;
-// 									}
-// 								}
-// 							});
-// 						}
-// 					}
-// 				}	
-// 			}
-// 			res.render('gadgets/list', { title: 'Gadgets: '+_result.length, gadgets : _result});  
-// 		})
-//     } else {
-//       res.render('index', { title: 'No devices' });  
-//     }
-//   })
-// });
-
-
-
-router.get('/find/', helper.ensureAuthenticated, function(req, res) {
-  gadgets.find({ "name" : {$regex : ".*"+req.query.q+".*", $options: 'i'}}).sort({brand:1}).toArray(function(_err,_result){
-    res.render('gadgets/list', { title: 'Gadgets: '+_result.length, gadgets : _result}); 
-  });
+router.post('/new', helper.ensureAuthenticated, helper.ensureAdmin, function(req, res) {
+  var gadget = {};
+  for(var key in req.body) {
+    gadget[key] = req.body[key];
+  }
+  gadgets.insert(gadget,function(_err,_result){
+     res.redirect('/gadgets/'+_result[0]._id+'/edit');
+  }) 
 });
 
 router.get('/:id', helper.ensureAuthenticated, helper.ensureAdmin, function(req, res) {
@@ -116,7 +81,7 @@ router.get('/:id', helper.ensureAuthenticated, helper.ensureAdmin, function(req,
 });
 
 
-router.get('/:id/edit', helper.ensureAuthenticated, helper.ensureAdmin, function(req, res) {
+router.get('/:id/edit',function(req, res) { //  helper.ensureAuthenticated, helper.ensureAdmin, 
   gadgets.findById(req.params.id,function(_err,_gadget){
     res.render('gadgets/edit', { title: _gadget.name , gadget : _gadget});  
   })
