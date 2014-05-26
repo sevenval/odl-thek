34673217d2d3ad9b14ccdac8a932ea4852766109
Ulrich Schmidt <ulrich.schmidt@sevenval.com>
2014-05-26 08:48:58 +0200

    correct the imprint spelling

--- routes/imprint.js
+++ routes/imprint.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
-    res.render('impress', { title: 'ODL: impress'});  
+    res.render('imprint', { title: 'ODL: imprint'});  
 });
 
 
