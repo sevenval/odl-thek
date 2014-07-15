1e47f084c09018433a91c68e23fbb0717476069c
Joachim Feldmann <joafeldmann@gmail.com>
2014-07-15 18:18:17 +0200

    fixes #14

--- models/gadget.js
+++ models/gadget.js
@@ -1,19 +1,14 @@
 /*jslint unparam: true, node: true, plusplus: true, nomen: true, indent: 2, todo: true */
 'use strict';
 
-var Mongoose  = require('mongoose');
-
+var Mongoose    = require('mongoose');
+var textSearch  = require('mongoose-text-search');
 
 var GadgetSchema = new Mongoose.Schema({
 
   hwid: {
     type: Number,
     unique: true,
-    required: true
-  },
-
-  name: {
-    type: String,
     required: true
   },
 
@@ -24,10 +19,11 @@ var GadgetSchema = new Mongoose.Schema({
 
   location: {
     type: String,
-    enum: [
-      'cologne',
-      'berlin'
-    ]
+    required: true
+    // enum: [
+    //   'cologne',
+    //   'berlin'
+    // ]
   },
 
   description: {
@@ -35,24 +31,34 @@ var GadgetSchema = new Mongoose.Schema({
   },
 
   brand: {
-    type: String
+    type: String,
+    required: true
   },
 
   model: {
-    type: String
+    type: String,
+    required: true
   },
 
   os: {
-    type: String
+    type: String,
+    required: true
   },
 
   type: {
     type: String,
+    required: true,
     enum: [
-      'mobile',
-      'tablet'
+      'Mobile',
+      'Tablet',
+      'Desktop',
+      'Tv',
+      'Incar',
+      'Console',
+      'Glasses',
+      'Watch'
     ],
-    default: 'mobile'
+    default: 'Mobile'
   },
 
   image: {
@@ -70,11 +76,30 @@ var GadgetSchema = new Mongoose.Schema({
     type: [ String ]
   }
 
+}, {
+  strict: true
 });
 
 
-GadgetSchema.virtual('detailedName').get(function () {
-  return this.name + ' (' + this.hwid + ')';
+// add a text index to the keywords array
+GadgetSchema.plugin(textSearch);
+GadgetSchema.index({ keywords: 'text' });
+
+
+// Create and update the gadget keyword search field on every save action
+GadgetSchema.pre('save', function (next) {
+  this.keywords = [];
+  this.keywords.push(this.model);
+  this.keywords.push(this.brand);
+  this.keywords.push(this.os);
+  this.keywords.push(this.type);
+  this.keywords.push(this.description);
+  next();
+});
+
+
+GadgetSchema.virtual('name').get(function () {
+  return this.brand + ' ' + this.model;
 });
 
 
@@ -86,24 +111,6 @@ GadgetSchema.virtual('hasImage').get(function () {
 GadgetSchema.virtual('imagePath').get(function () {
   return '/img/cache/' + this._id + '.jpg';
 });
-
-
-//
-// TODO: Add/update search keywords on every insert/update
-//
-// GadgetSchema.pre('init', function (next) {
-//   console.log('pre init');
-//   this.keywords = [
-//     this.name,
-//     'other'
-//   ];
-//   next();
-// });
-
-// GadgetSchema.pre('validate', function (next) {
-//   console.log('pre validate');
-//   next();
-// });
 
 
 module.exports = Mongoose.model('Gadget', GadgetSchema);
